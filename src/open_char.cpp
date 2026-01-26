88a3e961f8721cb327180153ab546374c8801d70
Ondrej Ille <ondrej.ille@gmail.com>
2026-01-26 15:22:18 +0100

    src: add parsing of command line arguments.

--- src/open_char.cpp
+++ src/open_char.cpp
@@ -1,6 +1,4 @@
 
-#include <iostream>
-
 #include <fmt/format.h>
 #include <fmt/printf.h>
 
@@ -9,10 +7,36 @@
 #include "open_char.h"
 #include "Context.h"
 
+
 open_char::Context *ctx;
 
-int TclInit(Tcl_Interp *interp)
+typedef enum {
+    ARG_HELP,
+    ARG_FILE,
+    ARG_LAST
+} t_arg_kind;
+
+typedef struct {
+    t_arg_kind      kind;
+    std::string     name;
+    std::string     val_name;
+    bool            present;
+    std::string     val;
+    std::string     desc;
+} t_cli_arg;
+
+static t_cli_arg argt[] = {
+    {ARG_HELP, "-help",   "",           false,   "",    "Display this help message"},
+    {ARG_FILE, "-file",   "<script>",   false,   "",    "Execute TCL script file"},
+    {ARG_LAST, "",        "",           false,   "",    ""}
+};
+
+int ShellInit(Tcl_Interp *interp)
 {
+    if (Tcl_Init(interp) == TCL_ERROR) {
+        return TCL_ERROR;
+    }
+
     ctx->interp_ = interp;
 
     Tcl_SetVar(interp, "tcl_prompt1",
@@ -24,15 +48,77 @@ int TclInit(Tcl_Interp *interp)
 
     open_char::RegisterTclCommands(ctx);
 
+    if (argt[ARG_FILE].present) {
+        fmt::printf("Evaluating file: %s\n", argt[ARG_FILE].val);
+        Tcl_EvalFile(interp, argt[ARG_FILE].val.c_str());
+    }
+
     return TCL_OK;
 }
 
-int main(int argc, char **argv)
+void PrintUsage()
+{
+    fmt::printf("OpenChar - Open Source library characterization tool\n");
+    fmt::printf("Usage: open_char [options]\n\n");
+
+    fmt::printf("Options:\n");
+    for (t_cli_arg *ent = argt; ent->kind != ARG_LAST; ent++)
+        fmt::printf("    %s %-10s %s\n", ent->name, ent->val_name, ent->desc);
+}
+
+int ParseArgs(int argc, char *argv[])
+{
+    int argi = 1;
+    while (argi < argc) {
+        bool found = false;
+
+        for (t_cli_arg *ent = argt; ent->kind != ARG_LAST; ent++) {
+            if (std::string(argv[argi]) != ent->name)
+                continue;
+
+            ent->present = true;
+            found = true;
+
+            if (ent->val_name == "")
+                break;
+
+            if (argi >= argc - 1) {
+                PrintUsage();
+                return TCL_ERROR;
+            }
+
+            argi++;
+            ent->val = std::string(argv[argi]);
+            break;
+        }
+
+        if (!found) {
+            fmt::printf("Error: Invalid argument %s\n", argv[argi]);
+            PrintUsage();
+            return TCL_ERROR;
+        }
+
+        argi++;
+    }
+
+    return TCL_OK;
+}
+
+int main(int argc, char *argv[])
 {
     ctx = new open_char::Context;
 
+    int parse_ret = ParseArgs(argc, argv);
+    if (parse_ret != TCL_OK)
+        return parse_ret;
+
+    if (argt[ARG_HELP].present) {
+        PrintUsage();
+        return 0;
+    }
+
     Tcl_FindExecutable(argv[0]);
-    Tcl_Main(argc, argv, TclInit);
+    Tcl_Main(argc, argv, ShellInit);
 
     // TODO: This is not called since "exit" is handled within Tcl_Main to exit
     //       the code!
