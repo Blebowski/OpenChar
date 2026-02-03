1102e55d7d1edcbdcced793cc2bcbb20bc14447a
Ondrej Ille <ondrej.ille@gmail.com>
2026-02-03 23:40:31 +0100

    test: extend logic table extraction test.

--- test/unit/ExtractLogicTableTest.cpp
+++ test/unit/ExtractLogicTableTest.cpp
@@ -5,15 +5,10 @@
 
 #include <cassert>
 
-int main()
+using namespace open_char;
+
+void test_inv(Context &ctx, Algorithms &algs)
 {
-    using namespace open_char;
-
-    Context ctx;
-    Algorithms algs(&ctx);
-
-    ctx.includes_.push_back(TEST_COMMON_DIR "/basic_gates.cdl");
-
     ctx.lib_.AddCell("INV");
     Cell &c1 = ctx.lib_.GetCell("INV");
 
@@ -24,11 +19,88 @@ int main()
 
     algs.GetLogicFunction(c1);
 
-    assert (c1.GetPins()["Y"].GetLogicTableEntry(0).first == 0);
-    assert (c1.GetPins()["Y"].GetLogicTableEntry(0).second == 1);
+    assert (c1.GetPin("Y").GetLogicTableEntry(0).first == 0);
+    assert (c1.GetPin("Y").GetLogicTableEntry(0).second == 1);
 
-    assert (c1.GetPins()["Y"].GetLogicTableEntry(1).first == 1);
-    assert (c1.GetPins()["Y"].GetLogicTableEntry(1).second == 0);
+    assert (c1.GetPin("Y").GetLogicTableEntry(1).first == 1);
+    assert (c1.GetPin("Y").GetLogicTableEntry(1).second == 0);
+}
+
+void test_nand2(Context &ctx, Algorithms &algs)
+{
+    ctx.lib_.AddCell("NAND2");
+    Cell &c1 = ctx.lib_.GetCell("NAND2");
+
+    c1.AddPin("Y",   PinDirection::OUT,     PinKind::DATA);
+    c1.AddPin("A",   PinDirection::IN,      PinKind::DATA);
+    c1.AddPin("B",   PinDirection::IN,      PinKind::DATA);
+    c1.AddPin("VDD", PinDirection::INOUT,   PinKind::PWR);
+    c1.AddPin("VSS", PinDirection::INOUT,   PinKind::PWR);
+
+    algs.GetLogicFunction(c1);
+
+    assert (c1.GetPin("Y").GetLogicTableEntry(0).first == 0b00);
+    assert (c1.GetPin("Y").GetLogicTableEntry(0).second == 1);
+
+    assert (c1.GetPin("Y").GetLogicTableEntry(1).first == 0b01);
+    assert (c1.GetPin("Y").GetLogicTableEntry(1).second == 1);
+
+    assert (c1.GetPin("Y").GetLogicTableEntry(2).first == 0b10);
+    assert (c1.GetPin("Y").GetLogicTableEntry(2).second == 1);
+
+    assert (c1.GetPin("Y").GetLogicTableEntry(3).first == 0b11);
+    assert (c1.GetPin("Y").GetLogicTableEntry(3).second == 0);
+}
+
+void test_half_adder(Context &ctx, Algorithms &algs)
+{
+    ctx.lib_.AddCell("HALF_ADDER");
+    Cell &c1 = ctx.lib_.GetCell("HALF_ADDER");
+
+    c1.AddPin("CO",  PinDirection::OUT,     PinKind::DATA);
+    c1.AddPin("S",   PinDirection::OUT,     PinKind::DATA);
+    c1.AddPin("A",   PinDirection::IN,      PinKind::DATA);
+    c1.AddPin("B",   PinDirection::IN,      PinKind::DATA);
+    c1.AddPin("VDD", PinDirection::INOUT,   PinKind::PWR);
+    c1.AddPin("VSS", PinDirection::INOUT,   PinKind::PWR);
+
+    algs.GetLogicFunction(c1);
+
+    assert (c1.GetPin("CO").GetLogicTableEntry(0).first == 0b00);
+    assert (c1.GetPin("CO").GetLogicTableEntry(0).second == 0);
+
+    assert (c1.GetPin("CO").GetLogicTableEntry(1).first == 0b01);
+    assert (c1.GetPin("CO").GetLogicTableEntry(1).second == 0);
+
+    assert (c1.GetPin("CO").GetLogicTableEntry(2).first == 0b10);
+    assert (c1.GetPin("CO").GetLogicTableEntry(2).second == 0);
+
+    assert (c1.GetPin("CO").GetLogicTableEntry(3).first == 0b11);
+    assert (c1.GetPin("CO").GetLogicTableEntry(3).second == 1);
+
+    assert (c1.GetPin("S").GetLogicTableEntry(0).first == 0b00);
+    assert (c1.GetPin("S").GetLogicTableEntry(0).second == 0);
+
+    assert (c1.GetPin("S").GetLogicTableEntry(1).first == 0b01);
+    assert (c1.GetPin("S").GetLogicTableEntry(1).second == 1);
+
+    assert (c1.GetPin("S").GetLogicTableEntry(2).first == 0b10);
+    assert (c1.GetPin("S").GetLogicTableEntry(2).second == 1);
+
+    assert (c1.GetPin("S").GetLogicTableEntry(3).first == 0b11);
+    assert (c1.GetPin("S").GetLogicTableEntry(3).second == 0);
+}
+
+int main()
+{
+    Context ctx;
+    Algorithms algs(&ctx);
+
+    ctx.includes_.push_back(TEST_COMMON_DIR "/basic_gates.cdl");
+
+    test_inv        (ctx, algs);
+    test_nand2      (ctx, algs);
+    test_half_adder (ctx, algs);
 
     return 0;
 }
