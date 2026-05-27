a36e3b2914357b2e0c07b9db6ad5ae421881a968
Ondrej Ille <ondrej.ille@gmail.com>
2026-05-27 11:00:31 +0200

    feat: add simplest possible stacktrace from glibc.

---
+++ src/Common/StackTrace.cpp
@@ -0,0 +1,18 @@
+#include <execinfo.h>
+#include <signal.h>
+
+#include <Utils.h>
+
+static void SegFaultHandler(int sig) {
+    void *array[10];
+    int size = backtrace(array, 10);
+
+    fprintf(stderr, "Error: signal %d:\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+    exit(1);
+}
+
+void StackTraceInit()
+{
+    signal(SIGSEGV, SegFaultHandler);
+}
