2495b0e66569b6d49a48cff20e525a998621cb1e
Ondrej Ille <ondrej.ille@gmail.com>
2026-05-27 19:16:03 +0200

    test,refact: use EQUAL_WITH_TOL in ComboDelayTest.

--- test/algorithms/MeasureComboDelayTest.cpp
+++ test/algorithms/MeasureComboDelayTest.cpp
@@ -35,17 +35,6 @@ void test_inv(Context &ctx, Algorithms &algs)
 
     Arc& arc = pin.GetArcs()[0];
 
-    // Default numbers for basic NMOS and PMOS in NGSPICE
-    assert (arc.GetFallDelays()[0][0] > 0.18 && arc.GetFallDelays()[0][0] < 0.19);
-    assert (arc.GetFallDelays()[0][1] > 2.72 && arc.GetFallDelays()[0][1] < 2.73);
-    assert (arc.GetFallDelays()[1][0] > 0.20 && arc.GetFallDelays()[1][0] < 0.21);
-    assert (arc.GetFallDelays()[1][1] > 2.75 && arc.GetFallDelays()[1][1] < 2.76);
-
-    assert (arc.GetRiseDelays()[0][0] > 0.09 && arc.GetRiseDelays()[0][0] < 0.10);
-    assert (arc.GetRiseDelays()[0][1] > 1.36 && arc.GetRiseDelays()[0][1] < 1.37);
-    assert (arc.GetRiseDelays()[1][0] > 0.11 && arc.GetRiseDelays()[1][0] < 0.12);
-    assert (arc.GetRiseDelays()[1][1] > 1.38 && arc.GetRiseDelays()[1][1] < 1.39);
-
     // Inputs from: 00000000
     // Inputs to:   00000001
     // Output from:        1
@@ -63,6 +52,16 @@ void test_inv(Context &ctx, Algorithms &algs)
     // | 0.09330 | 1.36650 |
     // | 0.11510 | 1.38750 |
     // ---------------------
+
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[0][0], 0.18410));
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[0][1], 2.72870));
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[1][0], 0.20790));
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[1][1], 2.75010));
+
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[0][0], 0.09330));
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[0][1], 1.36650));
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[1][0], 0.11510));
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[1][1], 1.38750));
 }
 
 int main()
