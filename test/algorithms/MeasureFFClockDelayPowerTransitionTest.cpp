dbbe640977d7eab6524d2671f9e5e54b3b99f664
Ondrej Ille <ondrej.ille@gmail.com>
2026-05-28 16:51:35 +0200

    test: add Power check to FFClockDelayPowerTransition test

--- test/algorithms/MeasureFFClockDelayPowerTransitionTest.cpp
+++ test/algorithms/MeasureFFClockDelayPowerTransitionTest.cpp
@@ -34,6 +34,95 @@ void test_dff_ckb_rb_sb(Context &ctx, Algorithms &algs)
     algs.MeasureFFClockDelay(c1);
     algs.MeasureFFClockTransition(c1);
     algs.MeasureFFClockPowers(c1);
+
+    Pin& pin = c1.GetPin("Q");
+    Arc& arc = pin.GetArcs()[0];
+
+    // cell_rise() {
+    //   index_1 ("0.010000, 0.100000")
+    //   index_2 ("0.020000, 0.300000")
+    //   values (
+    //     "0.127000, 0.702500"
+    //     "0.136600, 0.712000"
+    //   ) ;
+    // }
+
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[0][0], 0.127000));
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[0][1], 0.702500));
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[1][0], 0.136600));
+    assert (EQUAL_WITH_TOL(arc.GetRiseDelays()[1][1], 0.712000));
+
+    // rise_transition() {
+    //     index_1 ("0.010000, 0.100000")
+    //     index_2 ("0.020000, 0.300000")
+    //     values (
+    //       "0.075200, 0.986800"
+    //       "0.075100, 0.986700"
+    //     ) ;
+    // } /* end rise_transition */
+
+    assert (EQUAL_WITH_TOL(arc.GetRiseTransitions()[0][0], 0.075200));
+    assert (EQUAL_WITH_TOL(arc.GetRiseTransitions()[0][1], 0.986800));
+    assert (EQUAL_WITH_TOL(arc.GetRiseTransitions()[1][0], 0.075100));
+    assert (EQUAL_WITH_TOL(arc.GetRiseTransitions()[1][1], 0.986700));
+
+    // cell_fall() {
+    //   index_1 ("0.010000, 0.100000")
+    //   index_2 ("0.020000, 0.300000")
+    //   values (
+    //     "0.121800, 0.702500"
+    //     "0.131300, 0.711900"
+    //   ) ;
+    // } /* end cell_fall */
+
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[0][0], 0.121800));
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[0][1], 0.702500));
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[1][0], 0.131300));
+    assert (EQUAL_WITH_TOL(arc.GetFallDelays()[1][1], 0.711900));
+
+    // fall_transition() {
+    //   index_1 ("0.010000, 0.100000")
+    //   index_2 ("0.020000, 0.300000")
+    //   values (
+    //     "0.072200, 0.947200"
+    //     "0.072300, 0.947300"
+    //   ) ;
+    // } /* end fall_transition */
+
+    assert (EQUAL_WITH_TOL(arc.GetFallTransitions()[0][0], 0.072200));
+    assert (EQUAL_WITH_TOL(arc.GetFallTransitions()[0][1], 0.947200));
+    assert (EQUAL_WITH_TOL(arc.GetFallTransitions()[1][0], 0.072300));
+    assert (EQUAL_WITH_TOL(arc.GetFallTransitions()[1][1], 0.947300));
+
+    // rise_power() {
+    //     index_1 ("0.010000, 0.100000")
+    //     index_2 ("0.020000, 0.300000")
+    //     values (
+    //     "0.019818, 0.019733"
+    //     "0.021225, 0.021129"
+    //     ) ;
+    // } /* end rise_power */
+
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[0][0], 0.019818));
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[0][1], 0.019733));
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[1][0], 0.021225));
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[1][1], 0.021129));
+
+    // fall_power() {
+    //     index_1 ("0.010000, 0.100000")
+    //     index_2 ("0.020000, 0.300000")
+    //     values (
+    //     "0.045777, 0.444877"
+    //     "0.047278, 0.446323"
+    //     ) ;
+    // } /* end fall_power */
+
+    // TODO: Crosscheck if rise power and fall power are not swapped !
+
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[0][0], 0.045777));
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[0][1], 0.444877));
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[1][0], 0.047278));
+    assert (EQUAL_WITH_TOL(arc.GetRisePowers()[1][1], 0.446323));
 }
 
 int main()
