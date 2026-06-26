7f72e23d9173d23f6f24bd6f938e5b6f2d372f68
Ondrej Ille <ondrej.ille@gmail.com>
2026-06-26 16:47:47 +0200

    src, test: add license to the source file headers.

--- src/Tcl/TclCmdOpt.h
+++ src/Tcl/TclCmdOpt.h
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// OpenChar - VLSI library characterizer
+// Copyright (C) 2026  Ondrej Ille
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see
+// <https://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef TCL_CMD_OPT_H
 #define TCL_CMD_OPT_H
